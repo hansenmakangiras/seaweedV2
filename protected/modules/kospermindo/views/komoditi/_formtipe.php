7609bc55d7dfdff84f0750e610e2e4b5f9164607
hansenmakangiras <hansenmakangiras@gmail.com>
2016-08-11 00:54:22 +0800

    [ADD] Message View And Controller

--- protected/modules/kospermindo/views/komoditi/_formtipe.php
+++
@@ -1,7 +0,0 @@
-<?php
-  /**
-   * Created by PhpStorm.
-   * User: hanse
-   * Date: 6/17/2016
-   * Time: 11:00 AM
-   */
--- protected/modules/kospermindo/views/kordinator/view.php
+++
@@ -1,9 +0,0 @@
-<h1>View Book #<?php echo $model->id_petani; ?></h1>
-<?php $this->widget('zii.widgets.CDetailView', array(
-    'data'=>$model,
-    'attributes'=>array(
-        'id',
-        'nama_petani',
-        'id_koor'
-    ),
-)); ?>
